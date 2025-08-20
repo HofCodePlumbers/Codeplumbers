228f8f9b56654315a19d6dc25acb3a1f18c58a6d
Arvin Azad <azada36@farmingdale.edu>
2025-08-20 12:43:37 -0400

    Started working on Implementing K-means model into the project

--- Backend/ml/train_model.py
+++ Backend/ml/train_model.py
@@ -1,12 +1,14 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.cluster import KMeans
 from sklearn.metrics import classification_report
 import joblib
 import os
-
+import matplotlib.pyplot as plt
+import numpy as np
 # Load dataset
-df = pd.read_csv("Phishing_Websites_Data.csv")
+df = pd.read_csv("Backend/ml/Phishing_Websites_Data.csv")
 
 # Features and labels
 X = df.drop(columns=["Result"])
@@ -16,12 +18,17 @@ y = df["Result"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train model
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+model = KMeans(n_clusters=2)
+model.fit(X_train)
 
 # Evaluate
-y_pred = model.predict(X_test)
-print(classification_report(y_test, y_pred))
+y_pred = model.predict(X_train)
+#print(classification_report(y_test, y_pred))
+
+# Print clustering results
+labels = model.labels_
+clusterCount = np.bincount(labels)
+print("Cluster counts:", clusterCount)
 
 # Save model
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
